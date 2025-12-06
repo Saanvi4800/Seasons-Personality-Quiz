a19f1543e6d233725d5993862a9c33d68e70246c
Saanvi4800 <Saanvi4800@gmail.com>
2025-12-05 18:43:31 -0600

    Initial commit of the project

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
 var q3a3 = document.getElementById("q3a3");
 
+var displayResultButton = document.getElementById("displayResult")
+var restartButton = document.getElementById("restart")
+
+
 q1a1.addEventListener("click", winter);
 q1a2.addEventListener("click", spring);
 q1a3.addEventListener("click", summer);
@@ -32,29 +36,109 @@ q2a4.addEventListener("click", summer);
 
 q3a1.addEventListener("click", summer);
 q3a2.addEventListener("click", winter);
-q3a3.addEventListener("click", fall);
-q3a3.addEventListener("click", spring);
+q3a3.addEventListener("click", fallSpring);
 
-function summer(){
-  summerScore += 1;
-  questionCount += 1;
-  console.log("QC=" + questionCount + "ss=" + summerScore);
+restartButton.addEventListener("click", resetQuiz);
+
+function displayResult(){
+  document.getElementById("result").innerHTML = resultjs ;
 }
 
+function fallSpring(){
+  springScore += 1;
+  fallScore += 1;
+  questionCount += 1;
+  
+  if (questionCount == 3) {
+    finalResult();
+    displayResultButton.addEventListener("click", displayResult);
+  }
+}
+
+function summer(){
+  summerScore += 1;
+  questionCount += 1;
+  console.log("QC=" + questionCount + "ss=" + summerScore);
+
+  if (questionCount == 3) {
+    finalResult();
+    displayResultButton.addEventListener("click", displayResult);
+  }
+}
+
+
 function winter(){
-  winterScore += 1;
-  questionCount += 1;
-  console.log("QC=" + questionCount + "ws=" + winterScore);
+  winterScore += 1;
+  questionCount += 1;
+  console.log("QC=" + questionCount + "ws=" + winterScore);
+
+  if (questionCount == 3) {
+    finalResult();
+    displayResultButton.addEventListener("click", displayResult);
+  }
 }
 
 function spring(){
-  springScore += 1;
-  questionCount += 1;
-  console.log("QC=" + questionCount + "sps=" + springScore);
+  springScore += 1;
+  questionCount += 1;
+  console.log("QC=" + questionCount + "sps=" + springScore);
+
+  if (questionCount == 3) {
+    finalResult();
+    displayResultButton.addEventListener("click", displayResult);
+  }
 }
 
 function fall(){
-  fallScore += 1;
-  questionCount += 1;
-  console.log("QC=" + questionCount + "fs=" + fallScore);
+  fallScore += 1;
+  questionCount += 1;
+  console.log("QC=" + questionCount + "fs=" + fallScore);
+
+  if (questionCount == 3) {
+    finalResult();
+    displayResultButton.addEventListener("click", displayResult);
+  }
+}
+
+var resultjs = "You have a little bit of it all!"
+function finalResult(){
+  if (fallScore > winterScore){
+    if (fallScore > summerScore){
+      if (fallScore > springScore){
+        resultjs = "You are a Fall person"
+      }
+    }
+  }
+  if (winterScore > fallScore){
+    if (winterScore > summerScore){
+      if (winterScore > springScore){
+        resultjs = "You are a Winter person"
+      }
+    }
+  }
+  if (springScore > winterScore){
+    if (springScore > summerScore){
+      if (springScore > fallScore){
+        resultjs = "You are a Spring person"
+      }
+    }
+  }
+  if (summerScore > winterScore){
+    if (summerScore  > springScore){
+      if (summerScore  > fallScore){
+       resultjs = "You are a Summer person"
+      }
+    }
+  }
+}
+
+function resetQuiz(){
+  summerScore=0;
+  winterScore=0;
+  springScore=0;
+  fallScore=0;
+
+  questionCount=0;
+  document.getElementById("result").innerHTML = "You are a..." ;
+  resultjs = "You have a little bit of it all!"
 }
